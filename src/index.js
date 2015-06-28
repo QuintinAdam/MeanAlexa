505c0ea8921e2754c5421bbbecaeabfd59f7803f
Quintin Adam <quintinadam@live.com>
2015-06-28 11:52:14 -0600

    mean, nice,help intent

--- src/index.js
+++ src/index.js
@@ -22,51 +22,74 @@ var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-valu
 var AlexaSkill = require('./AlexaSkill');
 
 /**
- * HelloWorld is a child of AlexaSkill.
+ * MeanAlexa is a child of AlexaSkill.
  * To read more about inheritance in JavaScript, see the link below.
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
-var HelloWorld = function () {
+var MeanAlexa = function () {
     AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
-HelloWorld.prototype = Object.create(AlexaSkill.prototype);
-HelloWorld.prototype.constructor = HelloWorld;
+MeanAlexa.prototype = Object.create(AlexaSkill.prototype);
+MeanAlexa.prototype.constructor = MeanAlexa;
 
-HelloWorld.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("HelloWorld onSessionStarted requestId: " + sessionStartedRequest.requestId
+MeanAlexa.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("MeanAlexa onSessionStarted requestId: " + sessionStartedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any initialization logic goes here
 };
 
-HelloWorld.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    console.log("HelloWorld onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
-    var speechOutput = "Welcome to the Alexa Skills Kit, you can say hello";
+MeanAlexa.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("MeanAlexa onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    var speechOutput = "Welcome to the MeanAlexa extension, You can ask for a insult!";
     response.ask(speechOutput);
 };
 
-HelloWorld.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("HelloWorld onSessionEnded requestId: " + sessionEndedRequest.requestId
+MeanAlexa.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("MeanAlexa onSessionEnded requestId: " + sessionEndedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any cleanup logic goes here
 };
 
-HelloWorld.prototype.intentHandlers = {
+MeanAlexa.prototype.intentHandlers = {
     // register custom intent handlers
-    HelloWorldIntent: function (intent, session, response) {
-        response.tellWithCard("Hello World!", "Greeter", "Hello World!");
+    MeanAlexaIntent: function (intent, session, response) {
+        var personName = intent.slots.PersonName.value.toLowerCase();
+        var cardTitle = "Insult for " + personName;
+        var insult = "A fish has more brains than you.";
+        // var insult = getInsult;
+
+        if (insult) {
+          var insultWithName = personName + ", " + insult
+          response.tellWithCard(insultWithName, cardTitle, insultWithName);
+        } else {
+          response.ask("I'm sorry, " + personName +  " is not worth insulting What else can I help?");
+        }
+    },
+    NiceAlexaIntent: function (intent, session, response) {
+        var personName = intent.slots.PersonName.value.toLowerCase();
+        var cardTitle = "Motivation for " + personName;
+        var motivate = "Keep your eyes up and your head on straight!";
+        // var motivate = getMotivation;
+
+        if (motivate) {
+          var motivateWithName = personName + ", " + motivate
+          response.tellWithCard(motivateWithName, cardTitle, motivateWithName);
+        } else {
+          response.ask("I'm sorry, " + personName +  " is not worth motivating What else can I help?");
+        }
     },
     HelpIntent: function (intent, session, response) {
-        response.ask("You can say hello to me!");
+        response.ask("You can say insult name or motivate name. What would you like to do?");
     }
 };
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    // Create an instance of the HelloWorld skill.
-    var helloWorld = new HelloWorld();
-    helloWorld.execute(event, context);
+    // Create an instance of the MeanAlexa skill.
+    var meanAlexa = new MeanAlexa();
+    meanAlexa.execute(event, context);
 };
 
