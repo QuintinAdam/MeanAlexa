5854129e39b730c4a2a158b16522e62db8a1f37c
Quintin Adam <quintinadam@live.com>
2015-06-29 13:47:03 -0600

    adds nice alexa

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ var getInsult = function(insult) {
   var http = require("http");
   var url = "http://pleaseinsult.me/api?severity=random";
   var request = http.get(url, function (response) {
-      var buffer = "",
-          data;
+      var buffer = "", data;
 
       response.on("data", function (chunk) {
           buffer += chunk;
       });
 
       response.on("end", function (err) {
-
           data = JSON.parse(buffer);
           insult(data);
       });
@@ -18,6 +16,23 @@ var getInsult = function(insult) {
   });
 };
 
+var getMotivation = function(motivation) {
+  var http = require("http");
+  var url = "http://pleasemotivate.me/api";
+  var request = http.get(url, function (response) {
+      var buffer = "", data;
+
+      response.on("data", function (chunk) {
+          buffer += chunk;
+      });
+
+      response.on("end", function (err) {
+          data = JSON.parse(buffer);
+          motivation(data);
+      });
+
+  });
+};
 /**
  * App ID for the skill
  */
@@ -53,11 +68,7 @@ MeanAlexa.prototype.intentHandlers = {
     MeanAlexaIntent: function (intent, session, response) {
         var personName = intent.slots.PersonName.value.toLowerCase();
         var cardTitle = "Insult for " + personName;
-        var http = require("https");
-        var insult = '';
         getInsult(function(insult) {
-          // use the return value here instead of like a regular (non-evented) return value
-          console.log(insult.insult);
           insult = insult.insult;
           if (insult) {
             var insultWithName = personName + ", " + insult;
@@ -66,21 +77,19 @@ MeanAlexa.prototype.intentHandlers = {
             response.ask("I'm sorry, " + personName +  " is not worth insulting. What else can I help?");
           }
         });
-        // var insult = "A fish has more brains than you.";
-        // var insult = getInsult;
     },
     NiceAlexaIntent: function (intent, session, response) {
         var personName = intent.slots.PersonName.value.toLowerCase();
         var cardTitle = "Motivation for " + personName;
-        var motivate = "Keep your eyes up and your head on straight!";
-        // var motivate = getMotivation;
-
-        if (motivate) {
-          var motivateWithName = personName + ", " + motivate;
-          response.tellWithCard(motivateWithName, cardTitle, motivateWithName);
-        } else {
-          response.ask("I'm sorry, " + personName +  " is not worth motivating. What else can I help?");
-        }
+        getMotivation(function(motivation) {
+          motivation = motivation.motivation;
+          if (motivation) {
+            var motivationWithName = personName + ", " + motivation;
+            response.tellWithCard(motivationWithName, cardTitle, motivationWithName);
+          } else {
+            response.ask("I'm sorry, " + personName +  " is not worth motivating. What else can I help?");
+          }
+        });
     },
     HelpIntent: function (intent, session, response) {
         response.ask("You can say insult name or motivate name. What would you like to do?");
